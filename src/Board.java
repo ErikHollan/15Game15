831560e94ace34f5c346856aea6581737135a309
Erik Hollander <erik.hollander@yh.nackademin.se>
2021-10-26 19:50:46 +0200

    Bygger grund i board

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,34 @@
-public class Board {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class Board extends JFrame {
+
+    JPanel upper = new JPanel();
+    JPanel lower = new JPanel();
+    JButton play = new JButton("Nytt spel");
+    JButton cheat = new JButton("Fuska");
+
+    Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
+
+    Board() {
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        add(upper, BorderLayout.NORTH);
+        upper.add(play);
+        upper.add(cheat);
+        upper.setBackground(Color.ORANGE);
+
+        add(lower, BorderLayout.CENTER);
+        lower.setLayout(new GridLayout(4, 4));
+        lower.setBorder(border);
+        lower.setBackground(Color.cyan);
+
+
+    }
+
 }
