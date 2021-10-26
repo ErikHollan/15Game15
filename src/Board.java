6c0d748142d033af333205cf9b7510f89bc88638
Erik Hollander <erik.hollander@yh.nackademin.se>
2021-10-26 21:40:58 +0200

    Skapar buttons i Tile och börjar smått i Logik

--- src/Board.java
+++ src/Board.java
@@ -8,27 +8,32 @@ public class Board extends JFrame {
     JPanel lower = new JPanel();
     JButton play = new JButton("Nytt spel");
     JButton cheat = new JButton("Fuska");
-
     Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
 
     Board() {
+        //bottenplatta
         setSize(500, 500);
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+        Tile t = new Tile();
 
+        //upper
         add(upper, BorderLayout.NORTH);
         upper.add(play);
         upper.add(cheat);
         upper.setBackground(Color.ORANGE);
 
+        //lower
         add(lower, BorderLayout.CENTER);
         lower.setLayout(new GridLayout(4, 4));
         lower.setBorder(border);
         lower.setBackground(Color.cyan);
+        for (int i = 0; i < t.tiles.length; i++) {
+            lower.add(t.tiles[i]);
 
+        }
 
     }
-
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,5 +1,47 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Tile {
 
-    int i = 12;
+    JButton tiles[] = new JButton[16];
+    ArrayList<Integer> numberList = new ArrayList<>();
+    Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
 
+    Tile() {
+        for (int i = 0; i <= 15; i++) {
+            if (i < 15) {
+                numberList.add(i + 1);
+                tiles[i] = new JButton("" + numberList.get(i));
+                tiles[i].setBorder(border);
+                tiles[i].setFont(new Font("Serif", Font.PLAIN, 50));
+                tiles[i].addMouseListener(click);
+            } else {
+                tiles[i] = new JButton("");
+                tiles[i].setBorder(border);
+                tiles[i].setFont(new Font("Serif", Font.PLAIN, 50));
+                tiles[i].addMouseListener(click);
+            }
+        }
+
+    }
+
+
+    MouseAdapter click = new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) throws ClassCastException {
+            numberList.clear();
+            //Logik för knapptryck
+            /*
+            Logic l = new Logic();   //behöver man göra detta??
+            l.src=e.getSource();
+
+             */
+        }
+
+    };
 }
