6b065a18d3388dee5671f9db065f54f4be2cf028
Erik Hollander <erik.hollander@yh.nackademin.se>
2021-10-27 13:51:16 +0200

    ...

--- src/Tile.java
+++ src/Tile.java
@@ -4,15 +4,16 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Tile {
 
     JButton tiles[] = new JButton[16];
     ArrayList<Integer> numberList = new ArrayList<>();
     Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
+    JButton play = new JButton("Nytt spel");
+    JButton cheat = new JButton("Fuska");
 
-    Tile() {
+    public Tile() {
         for (int i = 0; i <= 15; i++) {
             if (i < 15) {
                 numberList.add(i + 1);
@@ -26,21 +27,31 @@ public class Tile {
                 tiles[i].setFont(new Font("Serif", Font.PLAIN, 50));
                 tiles[i].addMouseListener(click);
             }
+            play.addMouseListener(click);
+            cheat.addMouseListener(click);
         }
-
     }
-
 
     MouseAdapter click = new MouseAdapter() {
         @Override
         public void mouseClicked(MouseEvent e) throws ClassCastException {
+            gameLogic gameLogic = new gameLogic();
             numberList.clear();
-            //Logik för knapptryck
-            /*
-            Logic l = new Logic();   //behöver man göra detta??
-            l.src=e.getSource();
+            Object src = e.getSource();
+            int clicked = 0;
+            int empty = 0;
+            String sClicked = "";
+            String sEmpty = "";
 
-             */
+            if (src == cheat) {
+                gameLogic.cheat(tiles, numberList);
+            }
+            if (src == play) {
+                gameLogic.play(tiles, numberList);
+            }
+
+            gameLogic.move(tiles,src, clicked,empty,sClicked,sEmpty);
+
         }
 
     };
